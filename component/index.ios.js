458f7508f8a244052a9df99f94d52fbdcd301e09
Gill Peacegood <gill.peacegood@payliquid.com>
2020-03-24 16:55:40 +0000

    i've needed to do this seems to be a packager bug with RN 0.65.5

--- component/index.ios.js
+++ component/index.ios.js
@@ -1,9 +1,9 @@
 "use strict";
 
 import { AppState } from "react-native";
-import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "@react-native-community/push-notification-ios";
 
 module.exports = {
   state: AppState,
-  component: PushNotificationIOS
+  component: PushNotification
 };
